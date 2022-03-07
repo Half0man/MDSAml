73586c97c8bbc98a3c1827a7e74bc0045e576048
Half0man <gregor0ad@gmail.com>
2022-03-07 12:44:36 +0100

    1

---
+++ src/main/java/com/admsoft/mdsaml/MdsAmlApplication.java
@@ -0,0 +1,13 @@
+package com.admsoft.mdsaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdsAmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MdsAmlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/admsoft/mdsaml/MdsAmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admsoft.mdsaml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdsAmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
