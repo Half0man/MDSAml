27eea7c167b48a7e91dd5e03f850424afb829dda
Half0man <gregor0ad@gmail.com>
2022-03-15 15:05:51 +0100

    test przeszedł

--- src/test/java/com/admsoft/mdsaml/TransactionFileUploadTests.java
+++ src/test/java/com/admsoft/mdsaml/TransactionFileUploadTests.java
@@ -1,8 +1,11 @@
 package com.admsoft.mdsaml;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.Rollback;
@@ -11,6 +14,8 @@ import org.springframework.test.annotation.Rollback;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Rollback(false)
 public class TransactionFileUploadTests {
+    @Autowired
+    private TestEntityManager entityManager;
     @Autowired
     private TransactionFileRepository repository;
     @Test
@@ -22,6 +27,8 @@ public class TransactionFileUploadTests {
         transactionFile.setAmountOfMoney(2137);
         transactionFile.setTransactionDate("01.01.2020 21:37");
         transactionFile.setTitle("cash-in");
-
+        TransactionFile savedTransactionfile=repository.save(transactionFile);
+        TransactionFile existTransactionfile= entityManager.find(TransactionFile.class,savedTransactionfile.getId());
+        assertThat(transactionFile.getId()).isEqualTo(existTransactionfile.getId());
     }
 }
